4c9131c1901c91922be741a2d42c65f50991ad96
puayhiang <68947167+puayhiang@users.noreply.github.com>
2022-07-06 16:13:52 +0800

    error

--- src/contracts/TradeTrustERC721/TokenRegistryCompat.ts
+++ src/contracts/TradeTrustERC721/TokenRegistryCompat.ts
@@ -1,4 +1,5 @@
 import {TradeTrustERC721 as V2TradeTrustERC721} from "@govtechsg/token-registry-v2/dist/types/contracts";
 import {TradeTrustERC721 as V3TradeTrustERC721} from "@govtechsg/token-registry-v3";
+import "./compatibility/V2TradeTrustERC721"
 
 export type TokenRegistryCompat = V2TradeTrustERC721 | V3TradeTrustERC721;
--- src/contracts/TradeTrustERC721/compatibility/V2TradeTrustERC721.ts
+++ src/contracts/TradeTrustERC721/compatibility/V2TradeTrustERC721.ts
@@ -1,138 +1,42 @@
-// // import { TokenRegistryV2, TokenRegistryV3 } from "./TokenRegistry";
-// // import { V2TradeTrustERC721, V3TradeTrustERC721 } from "./TokenRegistry";
+// import { TradeTrustERC721 } from "@govtechsg/token-registry-v2/dist/types/contracts";
 
-// import { TradeTrustERC721 as V2TradeTrustERC721 } from "@govtechsg/token-registry-v2/dist/types/contracts";
-// import { TradeTrustERC721Interface } from "@govtechsg/token-registry-v3/dist/types/contracts/TradeTrustERC721";
-// import {
-//   BigNumber,
-//   // BigNumberish,
-//   BytesLike,
-//   CallOverrides,
-//   ContractTransaction,
-//   Overrides,
-//   PopulatedTransaction,
-// } from "ethers";
+import { TradeTrustERC721 } from "@govtechsg/token-registry-v2/dist/types/contracts";
 
-// import {
-//   BigNumberish
-// } from "@ethersproject/bignumber/lib/bignumber";
+declare module "@govtechsg/token-registry-v3/dist/types/contracts" {
+  interface TradeTrustERC721 {
+    test(test_input: string): string;
+  }
+}
 
-// // V2TradeTrustERC721.f
-// // declare module "@govtechsg/token-registry-v2/dist/types/contracts" {
-// //   interface TradeTrustERC721 {
-
-// //     interface: TradeTrustERC721Interface;
-
-// //     functions: {
-// //       mintTitle(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: Overrides & { from?: string | Promise<string> }
-// //       ): Promise<BigNumber>;
-
-// //       "mintTitle(address,address,uint256)"(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: Overrides & { from?: string | Promise<string> }
-// //       ): Promise<BigNumber>;
-
-// //     };
-
-// //     callStatic: {
+TradeTrustERC721.prototype.test = (test_input) => {
+  return test_input + ":"
+};
 
 
-// //       mintTitle(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: Overrides & { from?: string | Promise<string> }
-// //       ): Promise<ContractTransaction>;
+// V2TradeTrustERC721.prototype.estimateGas.mintTitle = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<BigNumber>{
+//   return Promise.resolve(BigNumber.from("0x0"));
+// };
 
-// //       "mintTitle(address,address,uint256)"(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: Overrides & { from?: string | Promise<string> }
-// //       ): Promise<ContractTransaction>;
-// //     };
-// //     estimateGas: {
-// //       mintTitle(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: CallOverrides
-// //       ): Promise<string>;
+// V2TradeTrustERC721.prototype.estimateGas["mintTitle(address,address,uint256)"] = V2TradeTrustERC721.prototype.estimateGas.mintTitle;
 
-// //       "mintTitle(address,address,uint256)"(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: CallOverrides
-// //       ): Promise<string>;
-// //     };
-
-// //     populateTransaction: {
-// //       mintTitle(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: Overrides & { from?: string | Promise<string> }
-// //       ): Promise<PopulatedTransaction>;
-
-// //     "mintTitle(address,address,uint256)"(
-// //         beneficiary: string,
-// //         holder: string,
-// //         tokenId: BigNumberish,
-// //         overrides?: Overrides & { from?: string | Promise<string> }
-// //       ): Promise<PopulatedTransaction>;
-// //     };
-
-// //     mintTitle(
-// //       beneficiary: string,
-// //       holder: string,
-// //       tokenId: BigNumberish,
-// //       overrides?: Overrides & { from?: string | Promise<string> }
-// //     ): Promise<ContractTransaction>;
-
-// //     "mintTitle(address,address,uint256)"(
-// //       beneficiary: string,
-// //       holder: string,
-// //       tokenId: BigNumberish,
-// //       overrides?: Overrides & { from?: string | Promise<string> }
-// //     ): Promise<ContractTransaction>;
-
-// //   }
-// // }
-
-
-// // V2TradeTrustERC721.prototype.estimateGas.mintTitle = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<BigNumber>{
-// //   return Promise.resolve(BigNumber.from("0x0"));
-// // };
-
-// // V2TradeTrustERC721.prototype.estimateGas["mintTitle(address,address,uint256)"] = V2TradeTrustERC721.prototype.estimateGas.mintTitle;
-
-
-// // V2TradeTrustERC721.prototype.callStatic.mintTitle = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<string>{
-// //   throw new Error("V2 Token Registry minting unsupported")
-// // };
-
-// // V2TradeTrustERC721.prototype.callStatic["mintTitle(address,address,uint256)"] = V2TradeTrustERC721.prototype.callStatic.mintTitle;
-
-
-// // V2TradeTrustERC721.prototype.populateTransaction.mintTitle = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<PopulatedTransaction>{
-// //   throw new Error("V2 Token Registry minting unsupported")
-// // };
-
-// // V2TradeTrustERC721.prototype.populateTransaction["mintTitle(address,address,uint256)"] = V2TradeTrustERC721.prototype.populateTransaction.mintTitle;
-
-// const run = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<ContractTransaction>{
+// V2TradeTrustERC721.prototype.callStatic.mintTitle = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<string>{
 //   throw new Error("V2 Token Registry minting unsupported")
 // };
 
-// V2TradeTrustERC721.prototype.mintTitle = run;
+// V2TradeTrustERC721.prototype.callStatic["mintTitle(address,address,uint256)"] = V2TradeTrustERC721.prototype.callStatic.mintTitle;
 
-// V2TradeTrustERC721.prototype["mintTitle(address,address,uint256)"] = run;
+// V2TradeTrustERC721.prototype.populateTransaction.mintTitle = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<PopulatedTransaction>{
+//   throw new Error("V2 Token Registry minting unsupported")
+// };
 
+// V2TradeTrustERC721.prototype.populateTransaction["mintTitle(address,address,uint256)"] = V2TradeTrustERC721.prototype.populateTransaction.mintTitle;
 
+// const run = function (beneficiary: string, holder: string, tokenId: BigNumberish, overrides?: Overrides & { from?: string | Promise<string> }) : Promise<ContractTransaction>{
+//   console.log("Run is Running")
+//   throw new Error("V2 Token Registry minting unsupported")
+//   // return new ContractTransaction.
+// };
+
+// TradeTrustERC721.prototype.mintTitle = run;
+
+// TradeTrustERC721.prototype["mintTitle(address,address,uint256)"] = run;
